ed7adf09f40d384d9590b3df4b7b1e560c761b67
Steve Clark <steve.a.clark48@gmail.com>
2022-02-10 10:19:44 -0600

    Created class entry

--- L02HandsOn/index.js
+++ L02HandsOn/index.js
@@ -1,13 +1,12 @@
 "use strict";
 // Entry class that uses Person, and has a firstName, lastName and birthday
-var Entry = /** @class */ (function () {
-    function Entry(firstName, lastName, birthday) {
+class Entry {
+    constructor(firstName, lastName, birthday) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.birthday = birthday;
     }
-    return Entry;
-}());
+}
 // Generic function that sorts any array and returns it
 function sort(argument) {
     return argument.sort();
