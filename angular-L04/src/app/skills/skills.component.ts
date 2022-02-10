6cfb564ff0994863136ae9cb65e193da692f9b6e
Steve Clark <steve.a.clark48@gmail.com>
2022-02-10 13:09:18 -0600

    added skill service import

--- angular-L04/src/app/skills/skills.component.ts
+++ angular-L04/src/app/skills/skills.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Skill } from '../models/skill';
-import { Level } from '../types/level.enum';
+
+import { SkillsService } from '../services/skills.service';
 
 @Component({
   selector: 'app-skills',
@@ -8,23 +9,7 @@ import { Level } from '../types/level.enum';
   styleUrls: ['./skills.component.css']
 })
 export class SkillsComponent implements OnInit {
-  skills: Skill[] = [
-    {
-      id: 0,
-      name: 'Abstract Mathematics',
-      level: Level.Expert
-    },
-    {
-      id: 1,
-      name: 'Philosophy',
-      level: Level.Advanced
-    },
-    {
-      id: 2,
-      name: 'Developing Software',
-      level: Level.Expert
-    }
-  ];
+  skills: Skill[];
 
   constructor() {}
 
