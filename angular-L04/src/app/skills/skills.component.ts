36a18a950b929d09b447077683ce73a730c42c60
Steve Clark <steve.a.clark48@gmail.com>
2022-02-10 13:04:06 -0600

    Added imports

--- angular-L04/src/app/skills/skills.component.ts
+++ angular-L04/src/app/skills/skills.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Skill } from '../models/skill';
+import { Level } from '../types/level.enum';
 
 @Component({
   selector: 'app-skills',
@@ -6,7 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./skills.component.css']
 })
 export class SkillsComponent implements OnInit {
-  skills = ['Abstract Mathematics', 'Philosophy', 'Developing software'];
+  skills: Skill[] = [
+    {
+      id: 0,
+      name: 'Abstract Mathematics',
+      level: Level.Expert
+    },
+    {
+      id: 1,
+      name: 'Philosophy',
+      level: Level.Advanced
+    },
+    {
+      id: 2,
+      name: 'Developing Software',
+      level: Level.Expert
+    }
+  ];
 
   constructor() {}
 
