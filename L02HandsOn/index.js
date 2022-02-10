4603c69ba2a40abf18bd74583c4d9ee62291278f
Steve Clark <steve.a.clark48@gmail.com>
2022-02-10 10:22:17 -0600

    added new person entry

--- L02HandsOn/index.js
+++ L02HandsOn/index.js
@@ -7,7 +7,12 @@ class Entry {
         this.birthday = birthday;
     }
 }
-// Generic function that sorts any array and returns it
-function sort(argument) {
-    return argument.sort();
+let newPerson = new Entry('Albert', 'Einstein', new Date('5/14/1879'));
+console.log('First Name: ' + newPerson.firstName + ', Last Name: ' + newPerson.lastName + ', Birthday: ' + newPerson.birthday);
+// Part 2
+let numbers = ['twelve', 'one-hundred', 'sixteen', 'forty-four', 'seventy-three', 'three-million'];
+function sortArray(array) {
+    return array.sort();
 }
+console.log('Array before sorting: ' + numbers);
+console.log('Array after sorting: ' + sortArray(numbers));
