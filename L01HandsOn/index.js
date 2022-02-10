ee8919d8a0dcaff6cc821becc4635f2efb658be2
Steve Clark <steve.a.clark48@gmail.com>
2022-02-10 10:07:06 -0600

    Ran code to ensure working

--- L01HandsOn/index.js
+++ L01HandsOn/index.js
@@ -1,17 +1,16 @@
 "use strict";
 // ContactCard class - implements Person and Contact interfaces:
-var ContactCard = /** @class */ (function () {
+class ContactCard {
     // Constructor that uses firstName and phoneNumber:
-    function ContactCard(firstName, phoneNumber) {
+    constructor(firstName, phoneNumber) {
         this.firstName = firstName;
         this.phoneNumber = phoneNumber;
     }
     // Method that sends message with the first name and phone number
-    ContactCard.prototype.sendMessage = function () {
+    sendMessage() {
         console.log("Name: " + this.firstName + ", Phone Number: " + this.phoneNumber);
-    };
-    return ContactCard;
-}());
+    }
+}
 // newPerson instance and calling sendMessage()
-var newPerson = new ContactCard('Clarke', 55512345678);
+let newPerson = new ContactCard('Clarke', 55512345678);
 newPerson.sendMessage();
