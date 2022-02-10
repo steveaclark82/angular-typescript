8cd2db06de52bd56f8b3e80045e1ca87a8759a00
Steve Clark <steve.a.clark48@gmail.com>
2022-02-10 13:11:29 -0600

    Changed constructor

--- angular-L04/src/app/skills/skills.component.ts
+++ angular-L04/src/app/skills/skills.component.ts
@@ -4,16 +4,19 @@ import { Skill } from '../models/skill';
 import { SkillsService } from '../services/skills.service';
 
 @Component({
-  selector: 'app-skills',
-  templateUrl: './skills.component.html',
-  styleUrls: ['./skills.component.css']
+    selector: 'app-skills',
+    templateUrl: './skills.component.html',
+    styleUrls: ['./skills.component.css']
 })
 export class SkillsComponent implements OnInit {
-  skills: Skill[];
+    skills: Skill[];
+    dataService: SkillsService;
 
-  constructor(private skillsService: SkillsService) {
-    this.skills = skillsService.skills;
-  }
+    constructor(private skillsService: SkillsService) {
+        this.dataService = skillsService;
+    }
 
-  ngOnInit() {}
+    ngOnInit() {
+        this.skills = this.dataService.skills;
+    }
 }
