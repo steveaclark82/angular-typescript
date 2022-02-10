dd727c6ad1d0edd00d3d564894ebd20ab832cce0
Steve Clark <steve.a.clark48@gmail.com>
2022-02-10 13:07:16 -0600

    added skillservice array

--- angular-L04/src/app/app.module.ts
+++ angular-L04/src/app/app.module.ts
@@ -4,15 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { SkillsComponent } from './skills/skills.component';
 
+// add the following import statement:
+import { SkillsService } from './services/skills.service';
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    SkillsComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  declarations: [AppComponent, SkillsComponent],
+  imports: [BrowserModule],
+  // add SkillsService to the providers array:
+  providers: [SkillsService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
