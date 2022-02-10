5eed1e084a047a50e131f6b8718c3bc839299730
Steve Clark <steve.a.clark48@gmail.com>
2022-02-10 10:06:12 -0600

    Created Javascript functions

--- L01HandsOn/index.js
+++ L01HandsOn/index.js
@@ -1,1 +1,17 @@
 "use strict";
+// ContactCard class - implements Person and Contact interfaces:
+var ContactCard = /** @class */ (function () {
+    // Constructor that uses firstName and phoneNumber:
+    function ContactCard(firstName, phoneNumber) {
+        this.firstName = firstName;
+        this.phoneNumber = phoneNumber;
+    }
+    // Method that sends message with the first name and phone number
+    ContactCard.prototype.sendMessage = function () {
+        console.log("Name: " + this.firstName + ", Phone Number: " + this.phoneNumber);
+    };
+    return ContactCard;
+}());
+// newPerson instance and calling sendMessage()
+var newPerson = new ContactCard('Clarke', 55512345678);
+newPerson.sendMessage();
