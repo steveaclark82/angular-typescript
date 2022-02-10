d8ec5719fd9f019bb1a2baeeeb22438d0e4b915c
Steve Clark <steve.a.clark48@gmail.com>
2022-02-10 11:05:20 -0600

    Added to html and css

--- L03HandsOn/AngularL03/src/app/app.component.ts
+++ L03HandsOn/AngularL03/src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularL03';
+  title = 'Mr. Bigglesworth';
+  city = 'New York';
+  tagline = 'Meow all day every day';
+  aboutMe = 'I am a city cat. I live with my humans in a beautiful apartment. I spend my time looking out the window, eating, and sleeping on my luxurious bed. My lovely, long white fur is brushed daily, so I am always looking my best.'
 }
