533602c0b97f2c00cffbde64c10c733e5664e9df
Steve Clark <steve.a.clark48@gmail.com>
2022-02-14 10:50:06 -0600

    fixwd import

--- angular-L6/src/app/modules/messag-form/message-form.component.ts
+++ angular-L6/src/app/modules/messag-form/message-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Message } from '../../../../shared/models/message';
+import { Message } from '../../shared/models/message';
 
 @Component({
   selector: 'app-message-form',
