94f6cd777a7ae6177836308a52b7414954b111e6
Steve Clark <steve.a.clark48@gmail.com>
2022-02-14 10:56:40 -0600

    update4d imports

--- angular-L6/src/app/app.module.ts
+++ angular-L6/src/app/app.module.ts
@@ -5,13 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
 import { GreetingComponent } from './shared/components/greeting/greeting.component';
-import { MessageFormComponent } from './shared/components/message-form/message-form.component';
 
 import { SkillsModule } from './modules/skills/skills.module';
+import { MessageFormModule } from './modules/message-form/message-form.module';
 
 @NgModule({
-    declarations: [AppComponent, GreetingComponent, MessageFormComponent],
-    imports: [BrowserModule, FormsModule, SkillsModule],
+    declarations: [AppComponent, GreetingComponent],
+    imports: [BrowserModule, FormsModule, SkillsModule, MessageFormModule],
     providers: [],
     bootstrap: [AppComponent]
 })
